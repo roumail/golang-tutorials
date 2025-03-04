95348492a7dcd4c312b033bfbb2f110dee91d351
Rohail Taimour <rohail.taimour@gmail.com>
2025-03-04 23:41:08 +0100

    Create folder structure required for tutorial

--- 02-greetings/greetings/greetings.go
+++ 02-greetings/greetings/greetings.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) (string,error) {
+func Hello(name string) (string, error) {
     // If no name was given, return an error with a message.
     if name == "" {
 	return "", errors.New("empty name")
